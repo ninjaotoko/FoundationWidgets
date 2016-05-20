f9c58766fe829b4fc89aba6d7a2b01a80a74c6c4
Xavier <xavier@link-b.com>
2016-05-20 18:13:46 -0300

    init

--- widgets.php
+++ widgets.php
@@ -8,21 +8,63 @@
     Author URI: -
  */
 
+// Block direct requests
+if ( !defined('ABSPATH') )
+	die('-1');
 
-add_action("widgets_init", array('Foundation_Orbit_Widget', 'register'));
+add_action("widgets_init", function(){
+    register_widget( 'Foundation_Orbit_Widget' );    
+});
 
 class Foundation_Orbit_Widget extends WP_Widget {
 
-    function Foundation_Orbit_Widget() {
+    function __construct() {
         // constructor
-        $options = array('classname' => 'foundation-orbit-widget',
-            'description' => 'Crea un widget con Orbit');
+        parent::__construct(
+            'Foundation_Orbit_Widget',
+            __('Foundation Orbit', 'text_domain'),
 
-       $this->WP_Widget('Foundation_Orbit_Widget', 'Foundation Orbit', $options);
+            array(
+                'classname' => 'foundation-orbit-widget', 
+                'description' => __('Crea un widget con Orbit', 'text_domain')
+            )
+        )
+
+        //$options = array('classname' => 'foundation-orbit-widget',
+        //    'description' => 'Crea un widget con Orbit');
+        //
+        //$this->WP_Widget('Foundation_Orbit_Widget', 'Foundation Orbit', $options);
     }
 
     function form($instance) {
         // saca el formulario de opciones en admin
+        echo(print_r($instance));
+        // Valores por defecto
+        $defaults = array('titulo' => 'Item', 'descripcion'=> '', 'url' => '');
+        // Se hace un merge, en $instance quedan los valores actualizados
+        $instance = wp_parse_args((array)$instance, $defaults);
+        // Cogemos los valores
+        $titulo = $instance['titulo'];
+        $descripcion = $instance['descripcion'];
+        $url = $instance['url'];
+        // Mostramos el formulario
+        ?>
+        <p>
+            Titulo
+            <input class="widefat" type="text" name="<?php echo $this->get_field_name('titulo');?>"
+                   value="<?php echo esc_attr($titulo);?>"/>
+        </p>
+        <p>
+            Descripcion
+            <input class="widefat" type="text" name="<?php echo $this->get_field_name('descripcion');?>"
+                   value="<?php echo esc_attr($descripcion);?>"/>
+        </p>
+        <p>
+            URL
+            <input class="widefat" type="text" name="<?php echo $this->get_field_name('url');?>"
+                   value="<?php echo esc_attr($url);?>"/>
+        </p>
+        <?php
     }
 
     function update($new_instance, $old_instance) {
